f16bbf1c7a37fd0301c4398b6c8a328b3f197894
Woorim Kim <wrkimlimi@gmail.com>
2023-07-10 03:47:11 +0900

    feature: QuestionList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,61 @@
-import Image from 'next/image'
+import QuestionList from "@/components/QusetionList";
 
 export default function Home() {
+  const data = {
+    content: [
+      {
+        questionId: 5,
+        recommend: 0,
+        view: 0,
+        answerCount: 0,
+        title: "this is test",
+        content: "this is test",
+        category: "MINISHELL",
+        createAt: "2023-07-06T13:18:32.167038",
+        nickname: "test123",
+      },
+      {
+        questionId: 1,
+        recommend: 0,
+        view: 0,
+        answerCount: 0,
+        title: "Grand Theft Auto: San Andreas",
+        content: "All we had to do, was follow the damn train, CJ!",
+        category: "MINISHELL",
+        createAt: "2023-07-06T00:48:01.132413",
+        nickname: "Big Smoke",
+      },
+    ],
+    pageable: {
+      sort: {
+        empty: false,
+        sorted: true,
+        unsorted: false,
+      },
+      offset: 0,
+      pageNumber: 0,
+      pageSize: 10,
+      unpaged: false,
+      paged: true,
+    },
+    last: true,
+    totalPages: 1,
+    totalElements: 2,
+    size: 10,
+    number: 0,
+    sort: {
+      empty: false,
+      sorted: true,
+      unsorted: false,
+    },
+    first: true,
+    numberOfElements: 2,
+    empty: false,
+  };
+
   return (
     <div>
-      
+      <QuestionList items={data.content}/>
     </div>
-  )
+  );
 }
---
+++ src/components/QusetionList.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { Box, List, ListItem, Link, Typography } from "@mui/material";
+import { Recommend, Pageview } from "@mui/icons-material";
+
+interface Question {
+  questionId: number;
+  recommend: number;
+  view: number;
+  answerCount: number;
+  title: string;
+  content: string;
+  category: string;
+  createAt: string;
+  nickname: string;
+}
+
+const QuestionList: React.FC<{ items: Question[] }> = (props) => {
+  return (
+    <List>
+      {props.items.map((question) => (
+        <ListItem key={question.questionId}>
+          <Box>
+            <Box>
+              <Typography>{question.category}</Typography>
+            </Box>
+            <Typography>
+              <Link href={`/detail/${question.questionId}`}>
+                {question.title}
+              </Link>
+            </Typography>
+            <Box>
+              <Typography>{question.nickname}</Typography>
+              <Typography>{question.createAt}</Typography>
+            </Box>
+            <Typography>{question.content}</Typography>
+            <Typography>
+              <Recommend>{question.recommend}</Recommend>
+              <Pageview>{question.view}</Pageview>
+            </Typography>
+          </Box>
+        </ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default QuestionList;
